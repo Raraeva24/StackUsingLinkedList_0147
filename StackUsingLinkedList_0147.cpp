f8ec59b3dd3d7eaff27e4f251a27535de75026fa
Raraeva24 <rara.eva.ft21@mail.co.id>
2025-05-23 09:28:32 +0700

    make case 2

--- StackUsingLinkedList_0147.cpp
+++ StackUsingLinkedList_0147.cpp
@@ -94,6 +94,15 @@ int main()
             cin >> value;
             stack.push(value); // push the entered value onto the stack
             break;
+        case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop(); // pop the top element from the stack
+            }
+            else
+            {
+                cout << "stack is empty, cannot pop" << endl;
+            }
+            break;
        
-    return 0;
 };
