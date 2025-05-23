8e73364a27dff0e24f3540f296e123ea8a87904c
Raraeva24 <rara.eva.ft21@mail.co.id>
2025-05-23 09:14:25 +0700

    implementasi fungsi push pada stack

--- StackUsingLinkedList_0147.cpp
+++ StackUsingLinkedList_0147.cpp
@@ -23,4 +23,9 @@ public:
         top = NULL; // initialize the stack with a null top pointer
     }
 
+    // push operation: insert an element onto the top of the stack
+    int push(int value) // operasi push
+    {
+        
+    }
 };
