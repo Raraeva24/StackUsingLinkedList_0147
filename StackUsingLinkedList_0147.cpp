7959e0a7e3445f39053e08ce5bb37f686a699590
Raraeva24 <rara.eva.ft21@mail.co.id>
2025-05-23 09:27:38 +0700

    make an option

--- StackUsingLinkedList_0147.cpp
+++ StackUsingLinkedList_0147.cpp
@@ -80,10 +80,10 @@ int main()
 
     while (choice != 4)
     {
-       
-        switch (choice)
-        {
-        
-    }
-    return 0;
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;        
 };
