7feb4c5ee92db569a5c7c4433360d319728dd4c5
Raraeva24 <rara.eva.ft21@mail.co.id>
2025-05-23 09:15:13 +0700

    menambahkan langkah-langkah push

--- StackUsingLinkedList_0147.cpp
+++ StackUsingLinkedList_0147.cpp
@@ -26,6 +26,11 @@ public:
     // push operation: insert an element onto the top of the stack
     int push(int value) // operasi push
     {
-        
+        Node *newNode = new Node(); // 1. allocate memory for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. set the new pointer of the new node
+        top = newNode;              // 4. update the top pointer into the new node
+        cout << "Push value: " << value << endl;
+        return value;
     }
 };
